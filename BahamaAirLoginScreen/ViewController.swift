e7d9977a3797b24d9bb157aed10fa10e376dd156
Stanislav Kotsar <minolin27@gmail.com>
2019-08-18 23:52:42 +0300

    add textfield obrder animation

--- BahamaAirLoginScreen/ViewController.swift
+++ BahamaAirLoginScreen/ViewController.swift
@@ -364,7 +364,17 @@ extension ViewController: UITextFieldDelegate {
             jump.toValue = textField.layer.position.y
             jump.duration = jump.settlingDuration
             textField.layer.add(jump, forKey: nil)
+            textField.layer.borderWidth = 3.0
+            textField.layer.borderColor = UIColor.clear.cgColor
             
+            let flash = CASpringAnimation(keyPath: "borderColor")
+            flash.damping = 7.0
+            flash.stiffness = 200.0
+            flash.fromValue = UIColor(red: 1.0, green: 0.27, blue: 0.0, alpha: 1.0).cgColor
+            flash.toValue = UIColor.white.cgColor
+            flash.duration = flash.settlingDuration
+            textField.layer.add(flash, forKey: nil)
+            textField.layer.cornerRadius = 5
         }
     }
 }
